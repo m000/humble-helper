12b44dd688da730103a973b802d1373826975ab7
Manolis Stamatogiannakis <mstamat@gmail.com>
2020-03-09 20:24:19 +0100

    Refactored content script and integrated it with the page action.

--- humble-helper.js
+++ humble-helper.js
@@ -1,38 +1,107 @@
+// Set some convenience globals.
 var manifest = browser.runtime.getManifest();
-console.log(manifest.name + ": started");
+var extension_id = manifest.applications.gecko.id;
+var display_order = [
+  "bundle", "display_order", "name", "selected", "steam", "drmfree",
+  "genres", "platforms", "developers",
+  "url_youtube", "url_steam", "url_image",
+  "msrp", "id",
+];
+var rawinfo = null;
 
-// show the presence of the extension
-document.body.style.border = "10px solid green";
+// Function that extracts information from the raw info dump.
+function parseRawInfo(rawinfo) {
+  var games_info    = rawinfo.contentChoiceData.initial.content_choices;
+  var display_order = rawinfo.contentChoiceData.initial.display_order;
+  var bundle        = rawinfo.contentChoiceData.initial.title;
+  var choices_made  = rawinfo.contentChoicesMade.initial.choices_made;
+  var choices_info = {};
 
-// Function to copy game information from a dictionary to clipboard,
-// in a format suitable for pasting into Excel.
-function copyToClipboard(game_info) {
-  var copied = "";
-  var display_order = [
-    "bundle", "display_order", "name", "selected", "steam", "drmfree",
-    "genres", "platforms", "developers",
-    "url_youtube", "url_steam", "url_image",
-    "msrp", "id",
-  ];
+  for (var g in games_info) {
+    // main game info and shorthands for long paths
+    var gi = games_info[g];
+    var ytl = gi.carousel_content["youtube-link"];
+    var steamid = gi.tpkds[0]["steam_app_id"];
 
-  game_info.forEach(function(g, i) {
-    display_order.forEach(function(k, j) {
-        copied += g[k] + "\t";
-    });
-    copied += "\n";
+    // sort stuff
+    gi.genres.sort();
+    gi.platforms.sort();
+
+    // assemble exported info dictionary
+    var info = {
+      "id": g,
+      "bundle": bundle,
+      "name": gi.title,
+      "genres": gi.genres.join(", "),
+      "platforms": gi.platforms.join(", "),
+      "msrp": gi["msrp|money"].amount + gi["msrp|money"].currency,
+      "developers": gi.developers.join(", "),
+      "url_youtube": (ytl.length > 0) ? "https://youtu.be/" + ytl[0] : "",
+      "url_steam": (steamid != "") ? "https://store.steampowered.com/app/" + steamid : "",
+      "url_image": gi.image,
+      "steam": gi.delivery_methods.includes("steam"),
+      "drmfree": gi.delivery_methods.includes("download"),
+      "selected": false,
+      "display_order": -1
+    };
+
+    choices_info[g] = info;
+  }
+
+  // update selected games
+  choices_made.forEach(function(g, i) {
+    choices_info[g].selected = true;
   });
 
-  navigator.clipboard.writeText(copied).then(function() {
-    window.alert(manifest.name + ": data copied to clipboard.");
-  }, function() {
-    // dump to web console if clipboard failed
-    window.alert(manifest.name + ": could not copy data to clipboard.");
-    console.log(copied);
+  // games in displayed order
+  var choices_info_o = [];
+  display_order.forEach(function(g, i) {
+    choices_info[g].display_order = i+1;
+    choices_info_o.push(choices_info[g]);
   });
+
+	return choices_info_o;
 }
 
-// Bundle data are temporarily loaded in a script tag and then removed from DOM.
-// Use an observer to intercept the removal.
+// Function that converts raw info to TSV format.
+function convertToTSV(rawinfo) {
+  var info = parseRawInfo(rawinfo);
+  var tsv = "";
+  info.forEach(function(g, i) {
+    display_order.forEach(function(k, j) {
+        tsv += g[k] + "\t";
+    });
+    tsv += "\n";
+  });
+	return {"format": "TSV", "count": info.length, "data": tsv};
+}
+
+// Function that converts raw info to JSON format.
+function convertToJSON(rawinfo) {
+  var info = parseRawInfo(rawinfo);
+	var json = JSON.stringify(info, null, 2);
+	return {"format": "JSON", "count": info.length, "data": json};
+}
+
+// Function to copy game information from a dictionary to clipboard.
+function copyToClipboard(contents) {
+	var success = false;
+  navigator.clipboard.writeText(contents).then(function() {
+    success = true;
+  }, function() {
+    success = false;
+    console.log(copied);
+  });
+	return success;
+}
+
+
+
+
+// show the presence of the extension
+document.body.style.border = "10px solid rgb(151, 177, 71)";
+
+// Observer used to intercept raw data from the DOM, before they are removed.
 // Content script must be set to run at document_end.
 const removal_observer = new MutationObserver(function(mutations, observer) {
   for(let m of mutations) {
@@ -44,68 +113,53 @@ const removal_observer = new MutationObserver(function(mutations, observer) {
         continue;
       }
 
-      var all_info = JSON.parse(m.removedNodes[0].innerText)["contentChoiceOptions"];
-      var games_info    = all_info.contentChoiceData.initial.content_choices;
-      var display_order = all_info.contentChoiceData.initial.display_order;
-      var bundle        = all_info.contentChoiceData.initial.title;
-      var choices_made  = all_info.contentChoicesMade.initial.choices_made;
-      var choices_info = {};
-
-      for (var g in games_info) {
-        // main game info and shorthands for long paths
-        var gi = games_info[g];
-        var ytl = gi.carousel_content["youtube-link"];
-        var steamid = gi.tpkds[0]["steam_app_id"];
-
-        // sort stuff
-        gi.genres.sort();
-        gi.platforms.sort();
-
-        // assemble exported info dictionary
-        var info = {
-          "id": g,
-          "bundle": bundle,
-          "name": gi.title,
-          "genres": gi.genres.join(", "),
-          "platforms": gi.platforms.join(", "),
-          "msrp": gi["msrp|money"].amount + gi["msrp|money"].currency,
-          "developers": gi.developers.join(", "),
-          "url_youtube": (ytl.length > 0) ? "https://youtu.be/" + ytl[0] : "",
-          "url_steam": (steamid != "") ? "https://store.steampowered.com/app/" + steamid : "",
-          "url_image": gi.image,
-          "steam": gi.delivery_methods.includes("steam"),
-          "drmfree": gi.delivery_methods.includes("download"),
-          "selected": false,
-          "display_order": -1
-        };
-
-        choices_info[g] = info;
-      }
-
-      // update selected games
-      choices_made.forEach(function(g, i) {
-        choices_info[g].selected = true;
-      });
-
-      // games in displayed order
-      var choices_info_o = [];
-      display_order.forEach(function(g, i) {
-        choices_info[g].display_order = i+1;
-        choices_info_o.push(choices_info[g]);
-      });
-
-      // copy data to clipboard
-      //console.log(choices_info_o);
-      copyToClipboard(choices_info_o);
-
-      // deregister once done
-      observer.disconnect();
-      console.log(manifest.name + ": done");
-      break;
+			var parsed = JSON.parse(m.removedNodes[0].innerText);
+			if ("contentChoiceOptions" in parsed) {
+				rawinfo = parsed.contentChoiceOptions;
+        observer.disconnect();
+        console.log(manifest.name + ": raw info retrieved");
+        break;
+		  }
     }
   }
 });
 
+// register observer for DOM changes
+console.log(manifest.name + ": waiting for raw info to appear");
 removal_observer.observe(document.body, { childList: true });
+
+// register listener for messages from page action script
+browser.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+	var error = true;
+	var response = "unknown";
+  const valid_buttons = ["humble-helper-copy-tsv", "humble-helper-copy-json", "humble-helper-copy-html"];
+
+  if (sender.id != extension_id) {
+		response = "Received message from unknown sender: " + sender.id;
+  } else if (!("humble_helper" in message)) {
+		response = "Received invalid message.";
+  } else if (!valid_buttons.includes(message.humble_helper)) {
+		response = "Invalid request in message: " + message.humble_helper;
+  } else if (rawinfo == null) {
+		response = "No game information have been found.";
+	} else {
+    // all good - convert data
+		var converted = null;
+		if (message.humble_helper == "humble-helper-copy-tsv") {
+			converted = convertToTSV(rawinfo);
+		} else if (message.humble_helper == "humble-helper-copy-json") {
+			converted = convertToJSON(rawinfo);
+		} else {
+			response = "Requested format not yet supported.";
+		}
+
+		// copy to clipboard
+		if (converted != null) {
+			error = copyToClipboard(converted.data);
+			response = (error ? "Failed to copy" : "Copied") + ` ${converted.count} items in ${converted.format} format to clipboard.`;
+		}
+  }
+  sendResponse({"error": error, "message": response});
+});
 
 // vim: tabstop=2 shiftwidth=2 softtabstop=2 expandtab cursorline
